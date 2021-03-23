34ed8d631633ac131be18fa99bcc97c0745a07dd
J <azrilnr280700@gmail.com>
2021-03-23 21:55:07 +0700

    Create TugasA.java

---
+++ Week 3/TugasA.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package week3;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class TugasA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Tugas A");
+        System.out.println();
+        
+        String[] namaLengkap = {"Azril", "Nur", "Rahman"};
+        
+        System.out.println("Namaku Adalah : " + namaLengkap[0] + " " + namaLengkap[1] + " " + namaLengkap[2]);
+        
+        System.out.println("");
+    }
+    
+}
