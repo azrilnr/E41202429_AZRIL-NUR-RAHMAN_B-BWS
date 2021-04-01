e322c50427969fbbf206bedb3cff3cbeac332e45
J <azrilnr280700@gmail.com>
2021-04-01 18:38:19 +0700

    Create TugaaB.java

---
+++ Week 4/TugaaB.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package week4;
+
+/**
+ *
+ * @author LENOVO
+ */
+;
+
+public class TugaaB {
+    public static void main(String[] args) {
+   
+        System.out.println("Tugas B");
+        System.out.println();
+        
+        System.out.print("Do__While");
+        System.out.println("Bilangan kelipatan 2 (1-100)");
+        System.out.println("======================================");
+        int hitung = 1;
+        do{
+            System.out.print(" " + hitung );
+            hitung = (hitung * 2);
+        }
+        while (hitung <= 100);
+        }    
+    }
+
